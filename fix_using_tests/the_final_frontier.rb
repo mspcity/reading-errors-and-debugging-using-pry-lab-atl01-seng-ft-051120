91f1a0162d29c2b012019d133c7f70c90f5b14e5
Youngmin Park <codepark2019@gmail.com>
2020-04-08 01:36:17 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -8,11 +8,12 @@ end
 def state_log(star_date)
 
   "Captain's Log, star date #{star_date}."
-  binding.pry
+
 end
 
 def engage
 
   puts state_log(date)
   date = generate_star_date
+  binding.pry
 end
